3f4fb548163aec41ea034ca7623b8825abc6d223
Ryotaro-Yabuzuka <ryotaro.yabuzuka@ibm.com>
2023-02-03 14:47:16 +0900

    Initial Commit

---
+++ Math-Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Math-Training
+//
+//  Created by Ryotaro Yabuzuka on 2023/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
